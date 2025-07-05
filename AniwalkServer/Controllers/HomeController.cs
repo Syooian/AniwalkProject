48ae16d43c7911f86fce3a062ef9b55fea0b27b2
Syooian <msn0126@hotmail.com>
2025-07-06 00:47:51 +0800

    從設定檔讀取Google Map API Key

--- AniwalkServer/Controllers/HomeController.cs
+++ AniwalkServer/Controllers/HomeController.cs
@@ -7,14 +7,19 @@ namespace AniwalkServer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.GoogleMapsApiKey = _configuration["GoogleMapAPIKey"];
+            Console.WriteLine($"Google Map API Key: {ViewBag.GoogleMapsApiKey}");
+
             return View();
         }
 
