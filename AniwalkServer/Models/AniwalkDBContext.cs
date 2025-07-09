47bdb0914586fe4b7ff9de840a54f4cea8d82d73
Syooian <msn0126@hotmail.com>
2025-07-09 14:13:31 +0800

    新增模型 "Countries"

---
+++ AniwalkServer/Models/AniwalkDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AniwalkServer.Models
+{
+    public class AniwalkDBContext : DbContext
+    {
+        public AniwalkDBContext(DbContextOptions<AniwalkDBContext> Options) : base(Options)
+        {
+
+        }
+
+        #region 描述資料表
+        /// <summary>
+        /// 國家
+        /// </summary>
+        public virtual DbSet<Countries> Countries { get; set; }
+        #endregion
+    }
+}
---
+++ AniwalkServer/Models/Countries.Partial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AniwalkServer.Models
+{
+    public partial class Countries
+    {
+        /// <summary>
+        /// 國家代碼
+        /// </summary>
+        [Key]
+        [HiddenInput]
+        public string CountryCode { get; set; } = null!;
+        /// <summary>
+        /// 國名
+        /// </summary>
+        public string CountryName { get; set; } = null!;
+        /// <summary>
+        /// 
+        /// </summary>
+        [HiddenInput]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
