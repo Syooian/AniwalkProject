c4ac72aaee4469af6c3d4b732a92ca54c18b0159
Syooian <msn0126@hotmail.com>
2025-07-15 16:09:58 +0800

    新增到訪紀錄的留言顯示

---
+++ AniwalkServer/ViewComponents/VC_Comment.cs
@@ -0,0 +1,40 @@
+﻿using AniwalkServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AniwalkServer.ViewComponents
+{
+    public class VC_Comment : ViewComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly AniwalkDBContext Context;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Context"></param>
+        public VC_Comment(AniwalkDBContext Context)
+        {
+            this.Context = Context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="VisitSN"></param>
+        /// <param name="IsChange">編輯 or 刪除</param>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync(int VisitSN, bool IsChange = false)
+        {
+            Console.WriteLine($"Invoke VC_Comment with VisitSN: {VisitSN}, IsChange: {IsChange}");
+
+            var Result = await Context.Comments.Where(V => V.SN == VisitSN).OrderByDescending(C => C.CommentDate).ToListAsync();
+
+            if (IsChange)
+                return View("Change", Result);
+
+            return View(Result);
+        }
+    }
+}
