371ce810282c9dbf926a75c0fd71841ea0d24f76
Syooian <msn0126@hotmail.com>
2025-07-18 14:19:42 +0800

    連線字串

--- AniwalkServerDBFirstTest/Program.cs
+++ AniwalkServerDBFirstTest/Program.cs
@@ -1,7 +1,13 @@
+﻿using AniwalkServerDBFirstTest.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AniwalkContext>(Options =>
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionStrings")));
 
 var app = builder.Build();
 
