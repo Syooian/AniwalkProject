248291dd54e0213267f236b5a42f3e13dd5490a8
Syooian <msn0126@hotmail.com>
2025-07-27 17:42:43 +0800

    新增回覆頁面的開啟流程

---
+++ AniwalkServer/ViewComponents/VC_Reply.cs
@@ -0,0 +1,46 @@
+﻿using AniwalkServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AniwalkServer.Data;
+
+namespace AniwalkServer.ViewComponents
+{
+    public class VC_Reply : ViewComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly AniwalkDBContext Context;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Context"></param>
+        public VC_Reply(AniwalkDBContext Context)
+        {
+            this.Context = Context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="VisitSN"></param>
+        /// <param name="ParentReplyID"></param>
+        /// <param name="IsChange">編輯 or 刪除</param>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync(int VisitSN, string ParentReplyID, bool IsChange = false)
+        {
+            Console.WriteLine($"Invoke VC_Reply with VisitSN : {VisitSN}, ParentReplyID : {ParentReplyID}, IsChange : {IsChange}");
+
+            var Result = await Context.Comments
+                .Where(V => V.SN == VisitSN)
+                .Include(V => V.Replies).ThenInclude(R => R.Member)
+                .Include(V => V.Replies).ThenInclude(R => R.ParentReply)
+                .OrderByDescending(C => C.CommentDate).ToListAsync();
+
+            if (IsChange)
+                return View("Change", Result);
+
+            return View(Result);
+        }
+    }
+}
