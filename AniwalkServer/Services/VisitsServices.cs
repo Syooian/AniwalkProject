aad8cbeed3e3eca772b934c91a2d3b0a0fbd9e49
Syooian <msn0126@hotmail.com>
2025-08-06 09:19:34 +0800

    加入Service檔

---
+++ AniwalkServer/Services/VisitsServices.cs
@@ -0,0 +1,46 @@
+﻿using AniwalkServer.Data;
+using AniwalkServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AniwalkServer.Services
+{
+    public class VisitsServices
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly AniwalkDBContext Context;
+
+        public VisitsServices(AniwalkDBContext Context)
+        {
+            this.Context = Context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Visits>> GetVisits()
+        {
+            return await Context.Visits.ToListAsync();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="SN"></param>
+        /// <returns></returns>
+        public async Task<Visits> GetVisit(int VisitSN)
+        {
+            var Visit = await Context.Visits
+                .Include(V => V.Member)
+                .Include(V => V.Anime)
+                .Include(V => V.Country)
+                .Include(V => V.VisitsPhotos)
+                .FirstOrDefaultAsync(V => V.SN == VisitSN);
+
+            return Visit;
+        }
+
+    }
+}
