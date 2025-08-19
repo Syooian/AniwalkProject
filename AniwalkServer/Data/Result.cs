d824740f089ccb5b33ccbc29fa8197572450bf9c
Syooian <msn0126@hotmail.com>
2025-08-19 10:58:39 +0800

    新增一個用來回傳結果的class

---
+++ AniwalkServer/Data/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace AniwalkServer.Data
+{
+    public class Result
+    {
+        /// <summary>
+        /// 回傳結果
+        /// </summary>
+        public ResultType Type { get; set; }
+        /// <summary>
+        /// 回傳訊息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Type"></param>
+        /// <param name="Message"></param>
+        public Result(ResultType Type = ResultType.Success, string Message = "")
+        {
+            this.Type = Type;
+            this.Message = Message;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ResultType
+    {
+        Success = 0,
+        Fail
+    }
+}
