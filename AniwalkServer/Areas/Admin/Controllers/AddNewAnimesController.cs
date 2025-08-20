7cd85ce2491a5f9312abf12ff94c66b758c20da3
Syooian <msn0126@hotmail.com>
2025-08-20 18:13:46 +0800

    移動Admin用的新增動畫建議Index和Edit，並搬移跟目錄原Controller的內容

--- AniwalkServer/Areas/Admin/Controllers/AddNewAnimesController.cs
+++ AniwalkServer/Areas/Admin/Controllers/AddNewAnimesController.cs
@@ -49,5 +49,79 @@ namespace AniwalkServer.Areas.Admin.Controllers
 
             return View(Result);
         }
+
+        // GET: AddNewAnimes/Edit/5
+        /// <summary>
+        /// 編輯新增動畫建議
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Authorize(Roles = Shared.Role_Admin)]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var addNewAnime = await AddNewAnimesServices.GetAddNewAnime(id);
+            if (addNewAnime == null)
+            {
+                return NotFound();
+            }
+
+            //// 將 Enum 轉換為 SelectList
+            //ViewData["Status"] = new SelectList(Enum.GetValues(typeof(AddNewAnimeStatusEnum))
+            //    .Cast<AddNewAnimeStatusEnum>()
+            //    .Select(e => new { Value = (int)e, Text =addNewAnime.GetStatusDisplayName( e.get .ToString() }), "Value", "Text");
+
+            return View(addNewAnime);
+        }
+
+        // POST: AddNewAnimes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// 編輯新增動畫建議
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="addNewAnime"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = Shared.Role_Admin)]
+        public async Task<IActionResult> Edit(int id, [Bind("SN,AnimeTitle,AddDate,Status,CloseDate,Note")] AddNewAnime addNewAnime)
+        {
+            if (id != addNewAnime.SN)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    switch (addNewAnime.Status)
+                    {
+                        case AddNewAnimeStatusEnum.AgreeToAdd:
+                        case AddNewAnimeStatusEnum.Disagree:
+                            addNewAnime.CloseDate = DateTime.Now;
+                            break;
+                    }
+
+                    Context.Update(addNewAnime);
+
+                    await Context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddNewAnimesServices.IsAddNewAnimeExists(addNewAnime.SN))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addNewAnime);
+        }
     }
 }
