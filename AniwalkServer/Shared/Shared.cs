82465b07a94859c52baf8b29bbe4934b0c2f0119
Syooian <msn0126@hotmail.com>
2025-08-30 16:16:03 +0800

    到訪記錄詳細檢視，取地圖資料的做法獨立成Function (因為要與Edit共用)

--- AniwalkServer/Shared/Shared.cs
+++ AniwalkServer/Shared/Shared.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using AniwalkServer.Data;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace AniwalkServer
 {
@@ -70,6 +73,19 @@ namespace AniwalkServer
             }
 
             return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ViewData"></param>
+        /// <returns></returns>
+        public static string GetMapDataJsonString(ViewDataDictionary ViewData)
+        {
+            if (ViewData.TryGetValue(ViewDataKeys.MapData, out object Data))
+                return JsonConvert.SerializeObject(Data);
+            else
+                return "";
         }
     }
 
