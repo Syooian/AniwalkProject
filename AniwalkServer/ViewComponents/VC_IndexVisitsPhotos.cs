bb2f8f0435f85081906c6729babb63adbbfb5bd4
Syooian <msn0126@hotmail.com>
2025-09-03 11:05:07 +0800

    新增ViewComponent

---
+++ AniwalkServer/ViewComponents/VC_IndexVisitsPhotos.cs
@@ -0,0 +1,28 @@
+﻿using AniwalkServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniwalkServer.ViewComponents
+{
+    public class VC_IndexVisitsPhotos : ViewComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        readonly VisitsServices VisitsServices;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="VisitsServices"></param>
+        public VC_IndexVisitsPhotos(VisitsServices VisitsServices)
+        {
+            this.VisitsServices = VisitsServices;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var Result = await VisitsServices.GetRandomVisitsPhotos(10);
+
+            return View(Result);
+        }
+    }
+}
