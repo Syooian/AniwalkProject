691876f974bf61c6245519abaf20894c6f21e7d2
Syooian <msn0126@hotmail.com>
2025-09-08 13:06:43 +0800

    新增選擇物件本體

---
+++ AniwalkServer/Views/Shared/_PageSelect.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AniwalkServer.Views.Shared
+{
+    public class _PageSelectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
