94a98fa75391a882df662ef1116deb3d8c78f190
Syooian <msn0126@hotmail.com>
2025-09-09 10:12:46 +0800

    修改Animes Model：新增"停用日期"屬性

---
+++ AniwalkServer/Migrations/20250909020548_AddAnimeDisabledDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AniwalkServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnimeDisabledDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DisabledDate",
+                table: "Animes",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisabledDate",
+                table: "Animes");
+        }
+    }
+}
