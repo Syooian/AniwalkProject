b30b42d0e16f36692f1035a4185ca7226d694d54
Syooian <msn0126@hotmail.com>
2025-09-11 14:39:09 +0800

    取動畫Header路徑的方法統一

---
+++ AniwalkServer/Shared/Shared.Animes.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace AniwalkServer
+{
+    public partial class Shared
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="AnimeID"></param>
+        /// <returns></returns>
+        public static string GetAnimeHeaderPhotoPath(string AnimeID)
+        {
+            var PhotosDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", AnimesPhotosRootPath, AnimeID);
+            string HeaderPhotoPath = null;
+            if (Directory.Exists(PhotosDir))
+            {
+                var PhotosPath = Directory.GetFiles(PhotosDir);
+
+                var HeaderIndex = Array.FindIndex(PhotosPath, N => N.Contains("Header"));
+                if (HeaderIndex != -1)
+                {
+                    HeaderPhotoPath = GetAnimesPhotosPath(AnimeID, Path.GetFileName(PhotosPath[HeaderIndex]));
+                }
+            }
+
+            return HeaderPhotoPath;
+        }
+    }
+}
