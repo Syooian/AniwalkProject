dc4dabe54bce629039b4a81e1f1db77c69721daa
Syooian <msn0126@hotmail.com>
2025-09-11 13:55:41 +0800

    新增和編輯動畫增加可選圖片

---
+++ AniwalkServer/Areas/Admin/Views/Animes/_HeaderPhotoContainer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AniwalkServer.Areas.Admin.Views.Animes
+{
+    public class _HeaderPhotoContainerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
